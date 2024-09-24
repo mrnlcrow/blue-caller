7d13ac38e6f1e074c747b9a309ec9723854cb3cc
mrnlbhattarai.mb <mrnlbhattarai.mb@gmail.com>
2024-09-24 09:51:51 +0545

    worker/customer forms created for login

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index , WorkerListView, WorkerDetailView
+from .views import index , WorkerListView, WorkerDetailView, WorkerCreateView, CustomerCreateView
 
 urlpatterns=[
     path('', WorkerListView.as_view(), name='worker-list'),
     path('worker/<int:pk>/',WorkerDetailView.as_view(), name='worker-detail'),
+    path('worker/create/',WorkerCreateView.as_view(), name='worker-create'),
+    path('customer/create/',CustomerCreateView.as_view(), name='customer-create'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
-from jobs.models import Worker
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from jobs.models import Worker, Customer
 
 def index(request):
     return HttpResponse("<h1>BlueCaller</h1>")
@@ -13,3 +14,22 @@ class WorkerListView(ListView):
 class WorkerDetailView(DetailView):
     model = Worker
 
+class WorkerCreateView(CreateView):
+    model = Worker
+    fields=['name','tagline','phone_number','bio']
+    success_url=reverse_lazy('worker-list')
+
+    def form_valid(self, form):
+        form.instance.owner=self.request.user
+        return super(WorkerCreateView, self).form_valid(form)
+
+# naming convention of the templates for these classes ..CreateView expect is model_form.html
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    fields=['name','phone_number']
+    success_url=reverse_lazy('worker-list')
+
+    def form_valid(self, form):
+        form.instance.owner=self.request.user
+        return super(WorkerCreateView, self).form_valid(form)    
