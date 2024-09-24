9c100323812d91d6aeddfaafefbd269cdecac32d
mrnlbhattarai.mb <mrnlbhattarai.mb@gmail.com>
2024-09-24 19:32:29 +0545

    landing page integrated

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import index , WorkerListView, WorkerDetailView, WorkerCreateView, CustomerCreateView, handle_login
+from django.views.generic import TemplateView
+
 
 urlpatterns=[
-    path('', WorkerListView.as_view(), name='worker-list'),
+    path('', TemplateView.as_view(template_name="landing/index.html"), name='landing-page'),
+    path('get-started/', WorkerListView.as_view(), name='worker-list'),
     path('account-setup/', handle_login, name='handle-login'),
     path('worker/<int:pk>/',WorkerDetailView.as_view(), name='worker-detail'),
     path('worker/create/',WorkerCreateView.as_view(), name='worker-create'),
